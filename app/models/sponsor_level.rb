5ba712539ed2cb78fc787666cb91cfed587cfe36
Amber Conville <crebma@gmail.com>
2015-02-28 22:04:30 -0500

    default order on sponsor levels

--- app/models/sponsor_level.rb
+++ app/models/sponsor_level.rb
@@ -1,4 +1,6 @@
 class SponsorLevel < ActiveRecord::Base
   belongs_to :event
   has_and_belongs_to_many :sponsors
+
+  default_scope -> { order("#{table_name}.order ASC") }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'coc' => 'coc#view'
 
+  resources :speakers
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
