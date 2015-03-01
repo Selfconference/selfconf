2904f66abca2fc0eff4aee675a214dbc24077f90
Amber Conville <crebma@gmail.com>
2015-03-01 13:05:06 -0500

    pointing speaker images at s3

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,6 @@ class Session < ActiveRecord::Base
   belongs_to :event
   belongs_to :room
   has_and_belongs_to_many :speakers
+
+  default_scope -> { order("#{table_name}.name ASC") }
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,6 @@
 class Speaker < ActiveRecord::Base
   belongs_to :event
   has_and_belongs_to_many :sessions
+
+  default_scope -> { order("#{table_name}.name ASC") }
 end
