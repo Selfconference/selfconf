35f067e861f9c552344f75091873b0bd6320ffb7
Amber Conville <crebma@gmail.com>
2015-05-11 20:49:03 -0400

    add organizers to event endpoints

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,15 @@
 class EventsController < ApplicationController
 
   def latest
-    render json: Event.latest
+    render json: Event.latest.to_json(:include => [:organizers])
   end
 
   def index
-    render json: Event.order('start_date DESC')
+    render json: Event.order('start_date DESC').to_json(:include => [:organizers])
   end
 
   def show
-    render json: Event.find(params[:id])
+    render json: Event.find(params[:id]).to_json(:include => [:organizers])
   end
 
 end
