1eb1a5eb83167a23d697870ad9ee21fa5d8a14dc
Amber Conville <crebma@gmail.com>
2015-05-29 09:03:39 -0400

    meant to highlight trevarthen derp

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,7 +16,7 @@ class Event < ActiveRecord::Base
     speakers << Speaker.find(4)
     speakers << Speaker.find(1)
     speakers << Speaker.find(3)
-    speakers << Speaker.find(9)
+    speakers << Speaker.find(13)
     speakers << Speaker.find(20)
     speakers << Speaker.find(35)
     speakers << Speaker.find(43)
