2af212d1d64e33e6a55c3e52565c91b32a6e26e7
Amber Conville <crebma@gmail.com>
2016-01-18 12:16:39 -0500

    trying to setup sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = {:host => 'selfconference.org'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :domain         => 'selfconference.org',
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
