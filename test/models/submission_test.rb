27ea6a3c9b053779061e5b6e22ffc63a7f495d60
Justin Searls <searls@gmail.com>
2016-01-26 13:51:23 -0500

    Add additional test cases

--- test/models/submission_test.rb
+++ test/models/submission_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  def test_not_voted_on_by_right_user_no_votes
-    submissions = Submission.not_voted_on_by(users(:jack), events(:gdi))
+  def test_not_voted_on_by_with_zero_votes
+    result = Submission.not_voted_on_by(users(:jack), events(:gdi))
 
-    assert_equal [submissions(:jill_gdi)], submissions
+    assert_equal [submissions(:jill_gdi)], result
+  end
+
+  def test_not_voted_on_by_with_same_user_vote
+    Vote.create!(user: users(:jack), submission: submissions(:jill_gdi))
+
+    result = Submission.not_voted_on_by(users(:jack), events(:gdi))
+
+    assert_equal [], result
+  end
+
+  def test_not_voted_on_by_with_different_user_vote
+    Vote.create!(user: users(:jane), submission: submissions(:jill_gdi))
+
+    result = Submission.not_voted_on_by(users(:jack), events(:gdi))
+
+    assert_equal [submissions(:jill_gdi)], result
   end
 end
