28692ce67cbf32a2a0c8232a608826699f9f65f9
Amber Conville <crebma@gmail.com>
2016-02-11 16:06:55 -0500

    include sponsors in sponsor levels

--- app/controllers/sponsor_levels_controller.rb
+++ app/controllers/sponsor_levels_controller.rb
@@ -3,7 +3,7 @@ class SponsorLevelsController < ApplicationController
   def index
     sponsor_levels = @event.sponsor_levels
     sponsor_levels = sponsor_levels.where('updated_at > ?', params[:from_date]) if params[:from_date]
-    render json: sponsor_levels
+    render json: sponsor_levels.to_json(:include => [:sponsors])
   end
 
   def show
