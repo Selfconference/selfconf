9c82e77dfcfccf62283007f88eb7bc10a467a5c7
Amber Conville <crebma@gmail.com>
2017-03-06 12:30:34 -0500

    Removing speakers json call

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -3,7 +3,6 @@ class SpeakersController < ApplicationController
   def index
     speakers = @event.speakers
     speakers = speakers.where('updated_at > ?', params[:from_date]) if params[:from_date]
-    render json: speakers.map(&:decorate).to_json(except: [:encrypted_password, :password_salt, :created_at, :updated_at, :reset_password_token, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :reset_password_sent_at])
   end
 
   def show
