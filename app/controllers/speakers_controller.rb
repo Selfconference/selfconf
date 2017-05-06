0ad2ec7ef64c871d17145901b5133aba035442d6
Amber Conville <crebma@gmail.com>
2017-05-06 15:23:22 -0400

    removing sessions from speakers response

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -9,8 +9,7 @@ class SpeakersController < ApplicationController
       format.json do
         speakers = @event.speakers
         speakers = speakers.where('updated_at > ?', params[:from_date]) if params[:from_date]
-        render json: speakers.to_json(include: { sessions: { only: [:id, :name, :abstract, :room_id, :slot_id] }},
-                                      only: [:id, :name, :bio, :headshot, :twitter, :event_id])
+        render json: speakers.to_json(only: [:id, :name, :bio, :headshot, :twitter, :event_id])
       end
     end
   end
@@ -18,8 +17,7 @@ class SpeakersController < ApplicationController
   def show
     render json: Speaker.find(params[:id])
                         .decorate
-                        .to_json(include: { sessions: { only: [:id, :name, :abstract, :room_id, :slot_id] }},
-                                 only: [:id, :name, :bio, :headshot, :twitter, :event_id])
+                        .to_json(only: [:id, :name, :bio, :headshot, :twitter, :event_id])
   end
 
 end
