266b1e443a896a0d33476bd3aa0a3b220574dd05
Amber Conville <crebma@gmail.com>
2019-02-04 20:19:55 -0500

    Refactor metric getting

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,13 +1,14 @@
 class MetricsController < ApplicationController
   def index
-    @expenses = MetricType.expense.metrics.where(event_id: @event)
-    @revenues = MetricType.revenue.metrics.where(event_id: @event)
-    @attendees = MetricType.attendee.metrics.where(event_id: @event)
-    @attendees_gender = MetricType.attendee_gender.metrics.where(event_id: @event)
-    @attendees_race = MetricType.attendee_race.metrics.where(event_id: @event)
-    @speakers = MetricType.speaker.metrics.where(event_id: @event)
-    @speakers_gender = MetricType.speaker_gender.metrics.where(event_id: @event)
-    @speakers_race = MetricType.speaker_race.metrics.where(event_id: @event)
+    metrics = @event.metrics
+    @expenses = metrics.where(metric_type: MetricType.expense)
+    @revenues = metrics.where(metric_type: MetricType.revenue)
+    @attendees = metrics.where(metric_type: MetricType.attendee)
+    @attendees_gender = metrics.where(metric_type: MetricType.attendee_gender)
+    @attendees_race = metrics.where(metric_type: MetricType.attendee_race)
+    @speakers = metrics.where(metric_type: MetricType.speaker)
+    @speakers_gender = metrics.where(metric_type: MetricType.speaker_gender)
+    @speakers_race = metrics.where(metric_type: MetricType.speaker_race)
 
     @expense_total = @expenses.map(&:amount).reduce(:+) || 0
     @revenue_total = @revenues.map(&:amount).reduce(:+) || 0
